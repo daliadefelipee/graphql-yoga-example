9bc666ca4eb7183fbb0fa97c5d78b2fe4c118e9c
daliadefelipee <dalbiexo@gmail.com>
2018-12-04 15:02:56 -0600

    :tada: - Simple Yoga server example

---
+++ index.js
@@ -0,0 +1,36 @@
+const fs = require ('fs');
+
+const { GraphQLServer } = require('graphql-yoga');
+
+const dinnerOptions = ['🍕', '🌭', '🍔', '🥗', '🍣'];
+
+const typeDefs = `
+  type Query {
+    whatsForDinner: String!
+  }
+`;
+
+const resolvers = {
+    Query: {
+        whatsForDinner: () => {
+            const idx = Math.floor(Math.random() * dinnerOptions.length);
+            const foodChoice = dinnerOptions[idx];
+            return `Tonight we eat ${foodChoice}`;
+        }
+    }
+};
+
+const opts = {
+    port: 7777,
+    endpoint: '/graphql'
+};
+
+fs.writeFileSync('schema.public.graphql', typeDefs);
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.start((opts) => {
+    console.log(
+        `😄 Server running at http://localhost:${opts.port}${opts.endpoint}`
+    );
+});
